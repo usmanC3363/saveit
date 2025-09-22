678ae210919bbdb7942c460f9f6b475bb341de55
usmanBoi <usmankith@gmail.com>
2025-09-23 01:35:55 +0500

    Initial commit from Create Next App

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-poppins",
+});
+
+
+export const metadata: Metadata = {
+  title: "SaveIt",
+  description: "The best storage solution you need",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${poppins.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
